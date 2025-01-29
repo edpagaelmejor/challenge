99bed7b1c397653b14d89a019e2389ab0c40259e
Edison Pardo <edisonpardo14389@gmail.com>
2025-01-28 20:26:43 -0500

    cambio de titulo

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//cambiar el titulo del 
+
+// Cambiar el título de la página
+function sortearAmigo() {
+    // Cambiar el título de la página cuando se hace clic en el botón
+    document.title = "¡Amigo Secreto Sorteado!";
+    
+    // Aquí puedes agregar el código que maneja el sorteo de amigos
+}
+
+
+
